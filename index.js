afacaf7ede54834abb276913e5f37e6fe462161e
Mrinank Kavuri <mrinank1318@gmail.com>
2024-10-19 00:31:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const fs = require('fs');
+
+const path = require('path');
+
+const filePath = path.join(__dirname, 'Practical-1.txt');
+const fileContent = fs.readFileSync(filePath, 'utf-8');
 const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.get('/', (req, res) => {
-  const Practical1 = fs.readFileSync('Practical-1.txt', 'utf8');
   res.json({
-    Practical1: Practical1,
+    Practical1: fileContent,
     Note: '\r\n indicates nxt line so remove that adn go to the nxt line'
   });
 });
